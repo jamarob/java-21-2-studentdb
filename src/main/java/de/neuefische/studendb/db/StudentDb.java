cae23fc7f4aa21852d7708481421912d212be7fa
Jan Robert <janmartinrobert@gmail.com>
2021-04-27 12:53:34 +0200

    Add add and remove

--- src/main/java/de/neuefische/studendb/db/StudentDb.java
+++ src/main/java/de/neuefische/studendb/db/StudentDb.java
@@ -27,4 +27,46 @@ public class StudentDb {
         int index = (int) Math.floor(Math.random() * students.length);
         return students[index];
     }
+
+    public void add(Student student) {
+        Student[] updatedStudents = new Student[students.length+1];
+
+        for (int i = 0; i < students.length; i++) {
+            updatedStudents[i] = students[i];
+        }
+
+        updatedStudents[updatedStudents.length-1] = student;
+
+        students = updatedStudents;
+    }
+
+    public void remove(Student student) {
+        int foundIndex = findIndex(student);
+        if(foundIndex < 0){
+            return;
+        }
+        Student[] updatedStudents = new Student[students.length-1];
+
+        for (int i = 0; i < students.length; i++) {
+            if(i < foundIndex){
+                updatedStudents[i] = students[i];
+            }
+            if(i > foundIndex){
+                updatedStudents[i-1] = students[i];
+            }
+        }
+
+        students = updatedStudents;
+    }
+
+    private int findIndex(Student student) {
+        for (int i = 0; i < students.length; i++) {
+            if(students[i].equals(student)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
 }
--- src/test/java/de/neuefische/studendb/db/StudentDbTest.java
+++ src/test/java/de/neuefische/studendb/db/StudentDbTest.java
@@ -3,11 +3,90 @@ package de.neuefische.studendb.db;
 import de.neuefische.studendb.model.Student;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class StudentDbTest {
+
+    private static Arguments[] provideTestAddArguments() {
+        return new Arguments[]{
+                Arguments.of(
+                        new Student[]{
+                                new Student("Student 1", "1"),
+                                new Student("Student 2", "2")
+                        },
+                        new Student[]{
+                                new Student("Student 1", "1"),
+                                new Student("Student 2", "2"),
+                                new Student("Jane", "42")
+                        }
+                ),
+                Arguments.of(
+                        new Student[]{},
+                        new Student[]{new Student("Jane", "42")}
+                )
+        };
+    }
+
+    private static Arguments[] provideTestRemoveArguments() {
+        return new Arguments[]{
+                Arguments.of(
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Jane", "42"),
+                                new Student("Peter", "23")
+                        },
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        }
+                ),
+                Arguments.of(
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        },
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        }
+                ),
+                Arguments.of(
+                        new Student[]{
+                                new Student("Jane", "42"),
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        },
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        }
+                ),
+                Arguments.of(
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23"),
+                                new Student("Jane", "42")
+                        },
+                        new Student[]{
+                                new Student("Hans", "12"),
+                                new Student("Peter", "23")
+                        }
+                ),
+                Arguments.of(
+                        new Student[]{},
+                        new Student[]{}
+                ),
+                Arguments.of(
+                        new Student[]{new Student("Jane", "42")},
+                        new Student[]{}
+                )
+        };
+    }
 
     @Test
     @DisplayName("list() returns all students in the db")
@@ -51,5 +130,34 @@ class StudentDbTest {
                 + "Student{name='Klaus', id='13'}\n"
                 + "Student{name='Franky', id='100'}\n";
         assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestAddArguments")
+    public void testAdd(Student[] givenStudents, Student[] expectedStudents) {
+        // Given
+        StudentDb studentDb = new StudentDb(givenStudents);
+        Student student = new Student("Jane", "42");
+
+        // When
+        studentDb.add(student);
+        Student[] actualStudents = studentDb.list();
+
+        // Then
+        assertArrayEquals(expectedStudents, actualStudents);
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestRemoveArguments")
+    public void testRemove(Student[] givenStudents, Student[] expectedStudents) {
+        // Given
+        StudentDb studentDb = new StudentDb(givenStudents);
+
+        // When
+        studentDb.remove(new Student("Jane", "42"));
+        Student[] actualStudents = studentDb.list();
+
+        // Then
+        assertArrayEquals(expectedStudents, actualStudents);
     }
 }
