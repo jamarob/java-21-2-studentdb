e05af4582ee47877779e3bf0f993c0aa8885459e
Jan Robert <janmartinrobert@gmail.com>
2021-04-27 10:02:34 +0200

    Add Student model

---
+++ src/main/java/de/neuefische/studendb/model/Student.java
@@ -0,0 +1,51 @@
+package de.neuefische.studendb.model;
+
+import java.util.Objects;
+
+public class Student {
+
+    private String name;
+    private String id;
+
+    public Student(String name, String id){
+        this.name = name;
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Objects.equals(name, student.name) && Objects.equals(id, student.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, id);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", id='" + id + '\'' +
+                '}';
+    }
+}
---
+++ src/test/java/de/neuefische/studendb/model/StudentTest.java
@@ -0,0 +1,85 @@
+package de.neuefische.studendb.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    @Test
+    @DisplayName("A students name can be set")
+    public void testSetAndGetName(){
+        // Given
+        Student student = new Student("Jane Doe", "42");
+
+        // When
+        student.setName("John Doe");
+        String actual = student.getName();
+
+        // Then
+        assertEquals("John Doe", actual);
+    }
+
+    @Test
+    @DisplayName("A students id can be set")
+    public void setAndGetId(){
+        // Given
+        Student student = new Student("Jane Doe", "42");
+
+        // When
+        student.setId("113");
+        String actual = student.getId();
+
+        // Then
+        assertEquals("113", actual);
+    }
+
+    @ParameterizedTest(name = "equals() of {0} and {1} is {2}")
+    @MethodSource("provideTestEqualsArguments")
+    public void testEquals(Student studentA, Student studentB, boolean expected){
+        // When
+        boolean actual = studentA.equals(studentB);
+
+        // Then
+        assertEquals(expected, actual);
+    }
+
+    private static Arguments[] provideTestEqualsArguments(){
+        return new Arguments[]{
+            Arguments.of(new Student("Jane", "42"), new Student("Jane","42"), true),
+            Arguments.of(new Student("Jane", "42"), new Student("John","42"), false),
+        };
+    }
+
+    @Test
+    @DisplayName("toString() returns a formatted student")
+    public void testToString(){
+        // Given
+        Student student = new Student("Jane", "42");
+
+        // When
+        String actual = student.toString();
+
+        // Then
+        String expected = "Student{name='Jane', id='42'}";
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("Students with same name and id have the same hash code")
+    public void testHashCode(){
+        // Given
+        Student studentA = new Student("Jane", "42");
+        Student studentB = new Student("Jane", "42");
+
+        // When
+        boolean actual = studentA.hashCode() == studentB.hashCode();
+
+        // Then
+        assertTrue(actual);
+    }
+}
